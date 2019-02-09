66f926c22ce26e9b0a7db54628e065571a7f62c2
Ivo Zubović <ivo.zubovic@gmail.com>
2019-02-09 17:48:16 +0100

    Unit tests for Degree and Radian

--- Source/Test/Core/Utility/Math/DegreeTest.cpp
+++ Source/Test/Core/Utility/Math/DegreeTest.cpp
@@ -1,8 +1,33 @@
+#include "Utility/Math/Degree.h"
+#include "gtest/gtest.h"
+#include "Utility/Math/SpMath.h"
+
 namespace sp
 {
 	namespace test {
-		void test_Degree_getValueDegree() {
+		TEST(test_Degree_getValueDegree) {
+			// Arrange
+			float const initialValueDegree{ 50.0F };
+			Degree const degree{ initialValueDegree };
 
+			// Act
+			float const resultValueDegree = degree.getValueDegrees();
+			
+			// Assert
+			ASSERT_EQ(initialValueDegree, resultValueDegree);
+		}
+
+		TEST(test_Degree_getValueRadian) {
+			// Arrange
+			float const initialValueDegree{ 50.0F };
+			Degree const degree{ initialValueDegree };
+
+			// Act
+			float const resultValueRadians = degree.getValueRadians();
+
+			// Assert
+			float const expectedValueRadians = initialValueDegree * PI / 180.0F;
+			ASSERT_EQ(expectedValueRadians, resultValueRadians);
 		}
 	}
 }
---
+++ Source/Test/Core/Utility/Math/RadianTest.cpp
@@ -0,0 +1,32 @@
+#include "gtest/gtest.h"
+#include "Utility/Math/Radian.h"
+#include "Utility/Math/SpMath.h"
+
+namespace sp {
+	namespace test {
+		TEST(test_Radian_getValueRadian) {
+			// Arrange
+			float const initialValueRadian{ 2.12F };
+			Radian const radian{ initialValueRadian };
+
+			// Act
+			float const resultValueRadians = radian.getValueRadians();
+
+			// Assert
+			ASSERT_EQ(initialValueRadian, resultValueRadians);
+		}
+
+		TEST(test_Radian_getValueDegree) {
+			// Arrange
+			float const initialValueRadian{ 5.1F };
+			Radian const radian{ initialValueRadian };
+
+			// Act
+			float const resultValueDegree = radian.getVlueDegrees();
+
+			// Assert
+			float const expectedValueDegree = initialValueRadian * 180.0F / PI;
+			ASSERT_EQ(expectedValueDegree, resultValueDegree);
+		}
+	}
+}
