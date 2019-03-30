70dce3ac218061dff1229fa4c9f0589161b81e0b
Ivo Zubović <ivo.zubovic@gmail.com>
2019-03-30 14:29:28 +0100

    Transpose matrix unit test

---
+++ Source/Test/Core/Utility/Math/Matrix4x4Test.cpp
@@ -0,0 +1,26 @@
+#include "Utility/Math/Matrix4x4.h"
+#include "gtest/gtest.h"
+
+namespace sp {
+	namespace test {
+		TEST(Matrix4x4, Transpose) {
+			// arrange
+			Matrix4x4 mat;
+			for (int i = 0; i < 4; ++i) {
+				for (int j = 0; j < 4; ++j) {
+					mat[i][j] = i * 10 + j;
+				}
+			}
+
+			// act
+			Matrix4x4 transposedMat = mat.transposed();
+
+			// assert
+			for (int i = 0; i < 4; ++i) {
+				for (int j = 0; j < 4; ++j) {
+					ASSERT_FLOAT_EQ(mat[i][j], transposedMat[j][i]);
+				}
+			}
+		}
+	}
+}
