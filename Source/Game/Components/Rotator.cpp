6900bfd14fee0bd18c758a1be8ce0f4f96afb447
Jure Čular <jure.cular@gmail.com>
2019-05-20 22:14:05 +0200

    Added missing includes

--- Source/Game/Components/Rotator.cpp
+++ Source/Game/Components/Rotator.cpp
@@ -2,6 +2,8 @@
 
 #include "Game/GameObject/GameObject.h"
 #include "Game/Components/Transform.h"
+#include "Core/Math/Radian.h"
+#include "Core/Math/SpMath.h"
 
 namespace sp {
 	Rotator::Rotator(GameObject * const gameObjectOwner) : GameObjectComponent{ gameObjectOwner } {
@@ -11,7 +13,8 @@ namespace sp {
 		this->passedTime += deltaTime;
 		auto transformComponentWeak = gameObjectOwner->getComponent<Transform>();
 		if (auto transformComponentShared = transformComponentWeak.lock()) {
-			transformComponentShared->setRotationEuler(Vector3{ 0.0F, sin(this->passedTime * (0.2F)) * 360.0F, 0.0F });
+			Radian radian(this->passedTime * (0.2F));
+			transformComponentShared->setRotationEuler(Vector3{ 0.0F, sin(radian) * 360.0F, 0.0F });
 		}
 	}
 }
